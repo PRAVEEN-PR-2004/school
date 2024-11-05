26e710baf4abfac47d43446e6012bca29a1895ff
PRAVEEN-PR-2004 <praveen2004ttp@gmail.com>
2024-11-06 00:25:41 +0530

    slide animation for home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,7 +2,7 @@ import React, { useEffect } from "react";
 
 import Banner from "../components/Banner";
 import TextCard from "../components/TextCard";
-
+import { Slide } from "react-awesome-reveal";
 import Spot from "../components/Spot";
 import Content from "../components/Content";
 
@@ -17,17 +17,35 @@ const Home = () => {
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
+
   return (
     <>
-      <Banner />
-      <Content />
-      <Spot />
-      <TextCard />
+      {/* Use Slide component with custom easing and duration for smoothness */}
+      <Slide triggerOnce duration={1000} direction="left" damping={0.3}>
+        <Banner />
+      </Slide>
 
-      <Slider1 />
+      <Slide triggerOnce duration={1000} direction="left" damping={0.3}>
+        <Content />
+      </Slide>
+
+      {/* <Slide triggerOnce duration={1000} direction="left" damping={0.3}> */}
+      <Spot />
+      {/* </Slide> */}
+
+      <Slide triggerOnce duration={1000} direction="left" damping={0.3}>
+        <TextCard />
+      </Slide>
+
+      <Slide triggerOnce duration={1000} direction="left" damping={0.3}>
+        <Slider1 />
+      </Slide>
 
       <CountUpComponent />
-      <Slider2 />
+
+      <Slide triggerOnce duration={1000} direction="left" damping={0.3}>
+        <Slider2 />
+      </Slide>
 
       <Footer />
     </>
